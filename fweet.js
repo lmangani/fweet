fc66d97cd67477a047958e8989bb0cefb255a5dd
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2015-10-24 01:18:22 +0200

    Init push

---
+++ fweet.js
@@ -0,0 +1,266 @@
+/*
+	==== fWeet! ====
+	fweet is a simple dweet-like clone for node.js and redis, based on elements of RetwisJS
+	Licensed under the GNU GPL v2.0 license (https://api.github.com/licenses/gpl-2.0)
+	(c) 2015 QXIP BV
+
+	API Methods:
+
+	POST	/post/:thing		Create a post for a Thing
+	GET	/get/latest/:thing	Read the latest post from a Thing
+	GET	/get/:thing		Read all the available posts from a Thing
+	GET	/get/timeline		Read all the available posts from a All the Things
+
+*/
+
+var version = "1.0b"
+
+// Init Modules & Functions
+var express = require('express');
+var app = express.createServer();
+var redis = require('redis');
+var r = redis.createClient(6379,'127.0.0.1');
+
+var Util = require('./lib/util.js');
+var User = require('./lib/user.js');
+var Post = require('./lib/post.js');
+
+var Res200 = { "status": "success", "data": "" };
+var Res500 = { "status": "failure", "error": "" };
+
+app.dynamicHelpers({
+   stash: function(req, res){
+      return req.stash; // Inspired by perl catalyst
+   }
+});
+
+app.configure(function(){
+   // Authenticator
+   app.use(express.basicAuth('qxip', 'qxip'));
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// API Calls
+
+app.all('*',function(req,res,next) {
+   req.stash = {};
+   next();
+});
+
+app.get('/', function(req,res) {
+   msg = Res200; msg.data = "[{ version: "+version+" }]"; res.send(msg);
+});
+
+app.all('/post/:thing', function(req,res) {
+   var thing = req.param('thing','');
+   var username = thing;
+   var password = "";
+      // Find or create Thing
+      r.get('thing:' + thing + ':id',function(err,val){
+         if (err) {
+	    msg = Res500; msg.error = err; res.send(msg);
+         } else if (val) {
+	    // Thing is valid!
+	    req.stash.user = {};
+	    req.stash.user.name = thing; req.stash.user.id = val;
+	    r.incr("global:nextPostId",function(err,pid) {
+	      var status = req.param('status').replace(/\n/,"");
+	      var post = [Post.currentVersion,pid,req.stash.user.id,req.stash.user.name,+new Date(),status].join('|');
+	
+	      r.set("post:"+pid,post)
+	      r.lpush("global:timeline",pid);
+	      r.ltrim("global:timeline",0,1000); //keep last 1000 only
+	
+	      r.smembers("uid:" + req.stash.user.id + ":followers",function(err,users) {
+	         if (err) {
+	            console.log("Could not retrieve followers");
+	            users = [];
+	         }
+	         users.push(req.stash.user.id);
+	         var multiAction = r.multi();
+	         for (uid in users) {
+	            multiAction.lpush("uid:"+users[uid]+":posts",pid);
+	         }
+	         multiAction.exec(function(err,replies) { 
+			 msg = Res200; msg.data = "[{ id: "+pid+" }]"; res.send(msg);
+		 });
+	      });
+	   });
+
+         } else {
+            User.register(thing,password,function(err,user,session) {
+               if (err) {
+		  msg = Res500; msg.error = err; res.send(msg);
+               } else {
+                  req.stash.user = user;
+                  // Use new Thing
+		    r.incr("global:nextPostId",function(err,pid) {
+		      if ( isJSON(req.param('status')) ) {
+		      	var status = JSON.stringify(req.param('status'));
+		      } else {
+			var status = req.param('status').replace(/\n/,"");
+		      }
+		      var post = [Post.currentVersion,pid,req.stash.user.id,req.stash.user.name,+new Date(),status].join('|');
+		
+		      r.set("post:"+pid,post)
+		      r.lpush("global:timeline",pid);
+		      r.ltrim("global:timeline",0,1000); //keep last 1000 only
+		
+		      r.smembers("uid:" + req.stash.user.id + ":followers",function(err,users) {
+		         if (err) {
+		            console.log("Could not retrieve followers");
+		            users = [];
+		         }
+		         users.push(req.stash.user.id);
+		         var multiAction = r.multi();
+		         for (uid in users) {
+		            multiAction.lpush("uid:"+users[uid]+":posts",pid);
+		         }
+		         multiAction.exec(function(err,replies) { 
+				 msg = Res200; msg.data = "[{ id: "+pid+", new: 1 }]"; res.send(msg);
+			});
+	              });
+	            });
+               }
+            });
+         }
+      });
+
+
+});
+
+app.get('/get/timeline', function(req,res) {
+   r.sort('global:users','DESC','LIMIT','0','10','GET','uid:*:thing',function(err,things) {
+      if (err) { msg = Res500; msg.error = error; return res.send(msg); }
+      req.stash.things = things || [];
+
+      var start = req.param('start',0);
+      var count = req.param('count',10);
+      req.stash.start = start;
+      req.stash.count = count;
+      User.getPosts(-1, start, count, function(err, posts) {
+         if (err) {console.log('error retrieving all posts : ' + err)}
+         req.stash.posts = posts;
+	 msg = Res200; msg.data = posts; res.send(msg);
+      });
+   });
+});
+
+app.get('/get/latest/:thing', function(req,res) {
+   r.get("thing:"+req.param('thing','')+":id",function(err,uid) {
+      if (err || !uid > 0) { msg = Res500; msg.error = err; return res.send(msg); }
+      var start = req.param('start',-1);
+      var count = req.param('count',0);
+      req.stash.start = start;
+      req.stash.count = count;
+      req.stash.uid = uid;
+      req.stash.thing = req.param('thing','');
+      User.getPosts(uid,start,count,function(err,posts) {
+         req.stash.posts = posts;
+	 msg = Res200; msg.data = posts; res.send(msg);
+      });
+   });
+});
+
+app.get('/get/:thing', function(req,res) {
+   r.get("thing:"+req.param('thing','')+":id",function(err,uid) {
+      if (err || !uid > 0) return res.send('not found!');
+      var start = req.param('start',0);
+      var count = req.param('count',50);
+      req.stash.start = start;
+      req.stash.count = count;
+      req.stash.uid = uid;
+      req.stash.thing = req.param('thing','');
+      User.getPosts(uid,start,count,function(err,posts) {
+         req.stash.posts = posts;
+	 msg = Res200; msg.data = posts; res.send(msg);
+      });
+   });
+});
+
+app.get('/del/latest/:thing', function(req,res) {
+   r.get("thing:"+req.param('thing','')+":id",function(err,uid) {
+      if (err || !uid > 0) return res.send('not found!');
+      var start = req.param('start',0);
+      var count = req.param('count',0);
+      req.stash.start = start;
+      req.stash.count = count;
+      req.stash.uid = uid;
+      req.stash.thing = req.param('thing','');
+      User.delPosts(uid,start,count,function(err,posts) {
+         req.stash.posts = posts;
+	 msg = Res200; msg.data = posts; res.send(msg);
+      });
+   });
+});
+
+
+app.get('/follow/:uid', function(req,res) {follow(req,res,req.param('uid',-1),true)});
+app.get('/unfollow/:uid', function(req,res) {follow(req,res,req.param('uid',-1),false)});
+
+function follow(req,res,uid,follow) {
+   if (!req.stash.user.isLoggedIn()) return res.redirect('/');
+   if (uid > 0 && uid != req.stash.user.id) {
+      if (follow) {
+         r.sadd("uid:"+uid+":followers",req.stash.user.id);
+         r.sadd("uid:"+req.stash.user.id+":following",uid);
+       } else {
+         r.srem("uid:"+uid+":followers",req.stash.user.id);
+         r.srem("uid:"+req.stash.user.id+":following",uid);
+      }
+   }
+   r.get('uid:'+uid+':name',function(err,name) {
+      res.redirect('/get/' + name);
+   });
+}
+
+app.post('/register', function(req,res) {
+   var username  = req.param('username','').trim();
+   var password  = req.param('password','').trim();
+
+   if (!username.length > 0 || !password.length > 0) {
+      res.render('error',{error : "Must supply all fields" });
+   } else {
+      r.get('username:' + username + ':id',function(err,val){
+         if (err) {
+            res.render('error',{error : "Error checking for existing username : " + err });
+         } else if (val) {
+            res.render('error',{error : "Username already in use, try something different!" });
+         } else {
+            User.register(username,password,function(err,user,session) {
+               if (err) {
+                  res.render('error',{error : 'Could not register user'});
+               } else {
+                  req.stash.user = user;
+                  res.send('registered user '+user);
+               }
+            });
+         }
+      });
+   }
+});
+
+
+function isJSON (jsonString){
+    try {
+        var o = JSON.parse(jsonString);
+        if (o && typeof o === "object" && o !== null) {
+            return o;
+        }
+    }
+    catch (e) { }
+    return false;
+};
+
+app.listen(80);
+
